3ce03869539102c012692d6b89683486572aa01b
Edoardo Di Vitale <edv.lavoro@gmail.com>
2020-05-19 17:15:44 +0200

    Upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+alert('Ciao!');
+var nome = prompt('Come ti chiami?');
+var cognome = prompt('E il tuo cognome?');
+var colore = prompt('Qual è il tuo colore preferito?');
+document.getElementById('pwd').innerHTML = 'La tua password è: ' + nome + cognome + colore + 19 + '... Usala a tuo rishio e pericolo.';
