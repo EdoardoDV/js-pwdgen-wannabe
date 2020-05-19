f8928225e2977d6db1b2478d5a8c6ba81b66b800
Edoardo Di Vitale <edv.lavoro@gmail.com>
2020-05-19 17:33:29 +0200

    Creazione variabile "password"

--- js/script.js
+++ js/script.js
@@ -2,4 +2,5 @@ alert('Ciao!');
 var nome = prompt('Come ti chiami?');
 var cognome = prompt('E il tuo cognome?');
 var colore = prompt('Qual è il tuo colore preferito?');
-document.getElementById('pwd').innerHTML = 'La tua password è: ' + nome + cognome + colore + 19 + '... Usala a tuo rishio e pericolo.';
+var password = nome + cognome + colore + 19;
+document.getElementById('pwd').innerHTML = 'La tua password è: ' + password + '... Usala a tuo rishio e pericolo.';
